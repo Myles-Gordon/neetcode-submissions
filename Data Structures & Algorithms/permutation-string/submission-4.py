743ed43c51a9f1888b827238233dca50fc301b7c
Myles-Gordon <mwexg5@gmail.com>
2026-06-19 15:06:15 -0400

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        s1let = [0]*26
+        for letter in s1:
+            s1let[ord(letter)-ord("a")] += 1
+        
+        res = [0]*26
+        for i in range(0, len(s1)):
+            res[ord(s2[i])-ord("a")] += 1
+
+        l = 0
+        r = len(s1)
+        while r<len(s2):
+            if res == s1let:
+                return True
+            else:
+                res[ord(s2[l])-ord("a")] -= 1
+                res[ord(s2[r])-ord("a")] += 1
+                l+=1
+                r+=1
+        
+        return res==s1let
